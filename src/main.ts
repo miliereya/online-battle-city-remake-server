1b5a36e3c566a9a00c3343d7315735215984d9fe
miliereya <105806526+miliereya@users.noreply.github.com>
2023-09-10 17:23:45 -0400

    renderer config

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,8 @@ import { AppModule } from './app.module'
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule)
-
 	app.enableCors({
-		origin: 'http://localhost:3000',
+		origin: process.env.CORS_ALLOW_ORIGIN ?? 'http://localhost:3000',
 		credentials: true,
 	})
 
